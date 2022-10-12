e54138f86921c01e8f4a1590d6b84ff7b8beecd8
Paul <ptb0890@gmail.com>
2022-10-12 22:56:54 +0100

    Need to include fft_common.f90

--- src/fft_mkl.f90
+++ src/fft_mkl.f90
@@ -32,7 +32,7 @@ module decomp_2d_fft
 
   ! common code used for all engines, including global variables, 
   ! generic interface definitions and several subroutines
-#include "fft_common.inc"
+#include "fft_common.f90"
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   !  This routine performs one-time initialisations for the FFT engine

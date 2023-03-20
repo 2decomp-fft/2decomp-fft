1df10e1bef453688203da7469fd389659611dff6
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2023-03-20 13:17:17 +0000

    Add format target to build system
    Runs the prettifier using a script for consistency

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,8 +4,12 @@
 #
 # Runs the prettifier over 2decomp sources.
 
+# Find 2decomp root
+SCRDIR=$( dirname -- "$0"; )
+D2DDIR=${SCRDIR}/../
+
 # fprettify options
 # --enable-replacements : Replaces relational operators, e.g. .lt. -> <
 # --c-relations : Use C-style relational operators, e.g. <=
 # -w 3 : Whitespace preset - operators, print/read, *, /, +, -
-fprettify --enable-replacements --c-relations -w 3 src/*f90 examples/*/*f90
+fprettify --enable-replacements --c-relations -w 3 ${D2DDIR}/src/*f90 ${D2DDIR}/examples/*/*f90

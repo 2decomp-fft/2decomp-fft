cd5a34253f86af61844f73d89142f9558e9e4981
Paul <ptb0890@gmail.com>
2023-06-13 22:04:23 +0100

    If compiler wrapper doesn't give MPI name try to use -show
    As a fallback option, use the -show argument to MPI to try and find
    the MPI family.
    
    Default is "Unknown"
    
    Currently this is only important for gfortran > 10 and dev builds
    with only OpenMPI supplying the necessary modules to turn up the
    warning/error flags fully. If the MPI family is MPICH or Unknown
    then less strict flags are used.

--- cmake/D2D_MPI.cmake
+++ cmake/D2D_MPI.cmake
@@ -1,6 +1,7 @@
 # MPI CMakeLists
 
 find_package(MPI REQUIRED)
+set(D2D_MPI_FAMILY "Unknown")
 # Stop if there is no MPI_Fortran_Compiler
 if (MPI_Fortran_COMPILER)
     message(STATUS "MPI_Fortran_COMPILER found: ${MPI_Fortran_COMPILER}")
@@ -8,14 +9,31 @@ if (MPI_Fortran_COMPILER)
     # Try to guess the MPI type to adapt compilation flags if necessary
     string(FIND "${MPI_Fortran_COMPILER}" "mpich" pos)
     if(pos GREATER_EQUAL "0")
-      set(FIND_MPICH TRUE)
+      set(D2D_MPI_FAMILY "MPICH")
       message(STATUS "MPI is MPICH type")
     endif()
     string(FIND "${MPI_Fortran_COMPILER}" "openmpi" pos)
     if(pos GREATER_EQUAL "0")
-      set(FIND_OMPI TRUE)
+      set(D2D_MPI_FAMILY "OMPI")
       message(STATUS "MPI is openMPI type")
     endif()
+
+    if (${D2D_MPI_FAMILY} STREQUAL "Unknown")
+      execute_process(COMMAND ${MPI_Fortran_COMPILER} "-show"
+	OUTPUT_VARIABLE mpi_show)
+      
+      string(FIND "${mpi_show}" "openmpi" pos)
+      if(pos GREATER_EQUAL "0")
+	set(D2D_MPI_FAMILY "OMPI")
+      endif()
+
+      string(FIND "${mpi_show}" "mpich" pos)
+      if(pos GREATER_EQUAL "0")
+	set(D2D_MPI_FAMILY "MPICH")
+      endif()
+    endif()
+
+    message(STATUS "MPI Compiler family: ${D2D_MPI_FAMILY}")
 else (MPI_Fortran_COMPILER)
     message(SEND_ERROR "This application cannot compile without MPI")
 endif(MPI_Fortran_COMPILER)

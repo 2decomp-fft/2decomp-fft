d3b4f9bb9b1ced27078764dc652ef8bc3034c52b
cflag <cedric.flageul@univ-poitiers.fr>
2023-09-11 14:07:36 +0200

    Some code is duplicated in the examples. Move it to a dedicated module

--- examples/io_test/io_var_test.f90
+++ examples/io_test/io_var_test.f90
@@ -6,9 +6,10 @@ program io_var_test
 
    use mpi
    use decomp_2d_constants
-   use decomp_2d_mpi
-   use decomp_2d
    use decomp_2d_io
+   use decomp_2d_mpi
+   use decomp_2d_testing
+   use decomp_2d
 #if defined(_GPU)
    use cudafor
    use openacc
@@ -21,8 +22,6 @@ program io_var_test
    integer :: p_row = 0, p_col = 0
    integer :: resize_domain
    integer :: nranks_tot
-   integer :: nargin, arg, FNLength, status, DecInd
-   character(len=80) :: InputFN
 
    real(mytype), parameter :: eps = 1.0E-7
 
@@ -63,44 +62,11 @@ program io_var_test
    ny = ny_base * resize_domain
    nz = nz_base * resize_domain
    ! Now we can check if user put some inputs
-   ! Handle input file like a boss -- GD
-   nargin = command_argument_count()
-   if ((nargin == 0) .or. (nargin == 2) .or. (nargin == 5)) then
-      do arg = 1, nargin
-         call get_command_argument(arg, InputFN, FNLength, status)
-         read (InputFN, *, iostat=status) DecInd
-         if (arg == 1) then
-            p_row = DecInd
-         elseif (arg == 2) then
-            p_col = DecInd
-         elseif (arg == 3) then
-            nx = DecInd
-         elseif (arg == 4) then
-            ny = DecInd
-         elseif (arg == 5) then
-            nz = DecInd
-         end if
-      end do
-   else
-      ! nrank not yet computed we need to avoid write
-      ! for every rank
-      call MPI_COMM_RANK(MPI_COMM_WORLD, nrank, ierror)
-      if (nrank == 0) then
-         print *, "This Test takes no inputs or 2 inputs as"
-         print *, "  1) p_row (default=0)"
-         print *, "  2) p_col (default=0)"
-         print *, "or 5 inputs as"
-         print *, "  1) p_row (default=0)"
-         print *, "  2) p_col (default=0)"
-         print *, "  3) nx "
-         print *, "  4) ny "
-         print *, "  5) nz "
-         print *, "Number of inputs is not correct and the defult settings"
-         print *, "will be used"
-      end if
-   end if
+   call decomp_2d_testing_init(p_row, p_col, nx, ny, nz)
 
    call decomp_2d_init(nx, ny, nz, p_row, p_col)
+
+   call decomp_2d_testing_log()
 
    ! also create a data set over a large domain
    call decomp_info_init(nx * 2, ny * 2, nz * 2, large)

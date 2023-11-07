14f8a88b5550a68d40136c1cdc66e99a783342f4
Stefano Rolfo <stefano.rolfo@stfc.ac.uk>
2023-11-07 08:02:16 +0000

    Add visualisation to grad3d example

--- examples/grad3d/grad3d.f90
+++ examples/grad3d/grad3d.f90
@@ -24,6 +24,8 @@ program grad3d
    integer :: resize_domain
    integer :: nranks_tot
    integer :: nargin, arg, FNLength, status, DecInd
+   integer :: ierror
+   logical :: passing, all_pass
    character(len=80) :: InputFN
 
    real(mytype), parameter :: lx = 1.0_mytype
@@ -31,17 +33,12 @@ program grad3d
    real(mytype), parameter :: lz = 1.0_mytype
 
    real(mytype) :: dx, dy, dz
+   real(mytype) :: error_ref 
 
    real(mytype), allocatable, dimension(:, :, :) :: phi1, phi2, phi3
    real(mytype), allocatable, dimension(:, :, :) :: dphiX, dphiY, dphiz
    real(mytype), allocatable, dimension(:, :, :) :: wk2, wk3
 
-   integer :: ierror
-
-   integer :: xlast, ylast, zlast
-
-
-   logical :: passing, all_pass
 
    call MPI_INIT(ierror)
    ! To resize the domain we need to know global number of ranks
@@ -93,14 +90,21 @@ program grad3d
    dx = lx/real(nx,mytype)
    dy = ly/real(ny,mytype)
    dz = lz/real(nz,mytype)
+   
+   if (nrank == 0) then
+      write (*, *) '-----------------------------------------------'
+      write (*, *) "Mesh Resolution ", nx , ny, nz 
+      write (*, *) '-----------------------------------------------'
+   end if
 
    call allocate_var()
    !$acc data create(phi2,phi3,wk2,wk3) copy(phi1,dphiX, dphiY, dphiZ)
    call init_phi()
    call compute_grad()
    call test_derX(dphiX)
-   call test_derY()
-   call test_derZ()
+   call test_derY(dphiY)
+   call test_derZ(dphiZ)
+   call write_data()
    !$acc end data
 
    if (nrank == 0) then
@@ -207,7 +211,7 @@ contains
 
       ! Compute Z derivative
       call transpose_y_to_z(phi2,phi3)
-      call derx(wk3,phi3,dz,zsize(1),zsize(2),zsize(3))
+      call derz(wk3,phi3,dz,zsize(1),zsize(2),zsize(3))
       call transpose_z_to_y(wk3,wk2)
       call transpose_y_to_x(wk2,dphiZ)
 
@@ -323,6 +327,8 @@ contains
       real(mytype) :: dphi, dphi_num
       real(mytype) :: error = 0._mytype
       real(mytype) :: err_all = 0._mytype
+      real(mytype) :: dphi2 = 0._mytype
+      real(mytype) :: sum_dphi2 = 0._mytype
       integer :: xe1, xe2, xe3
       integer :: xs1, xs2, xs3
 
@@ -341,7 +347,8 @@ contains
              z = (k + xs3 - 2) * dz
              y = (j + xs2 - 2) * dy
              x = (i + xs1 - 2) * dx
-             dphi = -2._mytype * twopi * sin (twopi * (x / lx)) * cos(twopi * (y / ly)) * sin(twopi * (z / lz))
+             dphi = -2._mytype * twopi * sin(twopi * (x / lx)) * cos(twopi * (y / ly)) * sin(twopi * (z / lz))
+             dphi2 = dphi2 + dphi * dphi
              dphi_num = df(i,j,k)
              error = error + (dphi-dphi_num)*(dphi-dphi_num)
           enddo
@@ -349,8 +356,10 @@ contains
       enddo
       !$acc end loop
       call MPI_ALLREDUCE(error, err_all, 1, real_type, MPI_SUM, MPI_COMM_WORLD, ierror)
-      err_all = sqrt(err_all) / (real(nx, mytype) * real(ny, mytype) * real(nz, mytype))
-
+      call MPI_ALLREDUCE(dphi2, sum_dphi2, 1, real_type, MPI_SUM, MPI_COMM_WORLD, ierror)
+      err_all = sqrt(err_all/sum_dphi2) / (real(nx, mytype) * real(ny, mytype) * real(nz, mytype))
+      error_ref = err_all
+      
       if (nrank == 0) then
          write (*, *) 'DX error / mesh point: ', err_all
       endif
@@ -360,20 +369,257 @@ contains
    !=====================================================================
    ! Calculate divergence using halo-cell exchange (data in Y-pencil)
    !=====================================================================
-   subroutine test_derY()
+   subroutine test_derY(df)
 
       implicit none
+      ! Arguments
+      real(mytype), intent(out), dimension(xsize(1),xsize(2),xsize(3)) :: df
 
+      integer :: i, j, k
+      real(mytype), parameter :: twopi=2._mytype*acos(-1._mytype)
+      real(mytype) :: x, y, z
+      real(mytype) :: dphi, dphi_num
+      real(mytype) :: error = 0._mytype
+      real(mytype) :: err_all = 0._mytype
+      real(mytype) :: dphi2 = 0._mytype
+      real(mytype) :: sum_dphi2 = 0._mytype
+      integer :: xe1, xe2, xe3
+      integer :: xs1, xs2, xs3
+
+      xe1 = xsize(1)
+      xe2 = xsize(2)
+      xe3 = xsize(3)
+      xs1 = xstart(1)
+      xs2 = xstart(2)
+      xs3 = xstart(3)
+
+      ! Initial velocity
+      !$acc parallel loop default(present) reduction(+:error)
+      do k=1,xe3
+        do j=1,xe2
+          do i=1,xe1
+             z = (k + xs3 - 2) * dz
+             y = (j + xs2 - 2) * dy
+             x = (i + xs1 - 2) * dx
+             dphi = -2._mytype * twopi * cos(twopi * (x / lx)) * sin(twopi * (y / ly)) * sin(twopi * (z / lz))
+             dphi2 = dphi2 + dphi * dphi
+             dphi_num = df(i,j,k)
+             error = error + (dphi-dphi_num)*(dphi-dphi_num)
+          enddo
+        enddo
+      enddo
+      !$acc end loop
+      call MPI_ALLREDUCE(error, err_all, 1, real_type, MPI_SUM, MPI_COMM_WORLD, ierror)
+      call MPI_ALLREDUCE(dphi2, sum_dphi2, 1, real_type, MPI_SUM, MPI_COMM_WORLD, ierror)
+      err_all = sqrt(err_all/sum_dphi2) / (real(nx, mytype) * real(ny, mytype) * real(nz, mytype))
+
+      if (nrank == 0) then
+         write (*, *) 'DY error / mesh point: ', err_all
+      endif
+
+      if (abs(err_all-error_ref) > 1.0e-5_mytype) all_pass = .false.
 
    end subroutine test_derY
 
    !=====================================================================
    ! Calculate divergence using halo-cell exchange (data in Z-pencil)
    !=====================================================================
-   subroutine test_derZ()
+   subroutine test_derZ(df)
 
       implicit none
+      ! Arguments
+      real(mytype), intent(out), dimension(xsize(1),xsize(2),xsize(3)) :: df
+
+      integer :: i, j, k
+      real(mytype), parameter :: twopi=2._mytype*acos(-1._mytype)
+      real(mytype) :: x, y, z
+      real(mytype) :: dphi, dphi_num
+      real(mytype) :: error = 0._mytype
+      real(mytype) :: err_all = 0._mytype
+      real(mytype) :: dphi2 = 0._mytype
+      real(mytype) :: sum_dphi2 = 0._mytype
+      integer :: xe1, xe2, xe3
+      integer :: xs1, xs2, xs3
+
+      xe1 = xsize(1)
+      xe2 = xsize(2)
+      xe3 = xsize(3)
+      xs1 = xstart(1)
+      xs2 = xstart(2)
+      xs3 = xstart(3)
+
+      ! Initial velocity
+      !$acc parallel loop default(present) reduction(+:error)
+      do k=1,xe3
+        do j=1,xe2
+          do i=1,xe1
+             z = (k + xs3 - 2) * dz
+             y = (j + xs2 - 2) * dy
+             x = (i + xs1 - 2) * dx
+             dphi = 2._mytype * twopi * cos(twopi * (x / lx)) * cos(twopi * (y / ly)) * cos(twopi * (z / lz))
+             dphi2 = dphi2 + dphi * dphi
+             dphi_num = df(i,j,k)
+             error = error + (dphi-dphi_num)*(dphi-dphi_num)
+          enddo
+        enddo
+      enddo
+      !$acc end loop
+      call MPI_ALLREDUCE(error, err_all, 1, real_type, MPI_SUM, MPI_COMM_WORLD, ierror)
+      call MPI_ALLREDUCE(dphi2, sum_dphi2, 1, real_type, MPI_SUM, MPI_COMM_WORLD, ierror)
+      err_all = sqrt(err_all/sum_dphi2) / (real(nx, mytype) * real(ny, mytype) * real(nz, mytype))
+
+      if (nrank == 0) then
+         write (*, *) 'DZ error / mesh point: ', err_all
+      endif
+
+      if (abs(err_all-error_ref) > 1.0e-5_mytype) all_pass = .false.
 
    end subroutine test_derZ
+   !=====================================================================
+   ! Write of the results (all data are in X-pencil)
+   !=====================================================================
+   subroutine write_data()
+    
+      use decomp_2d_io
 
+      implicit none
+    
+      character(len=*), parameter :: io_name = "grad-io"
+#ifndef ADIOS2
+      logical :: dir_exists
+#endif
+      
+      !$acc update self (phi1)
+      !$acc update self (dphiX)
+      !$acc update self (dphiY)
+      !$acc update self (dphiZ)
+#ifndef ADIOS2
+      if (nrank == 0) then
+         inquire (file="out", exist=dir_exists)
+         if (.not. dir_exists) then
+            call execute_command_line("mkdir out 2> /dev/null")
+         end if
+      end if
+#endif
+
+      ! Standard I/O pattern - file per field
+#ifdef ADIOS2
+      call decomp_2d_open_io(io_name, "out", decomp_2d_write_mode)
+      call decomp_2d_start_io(io_name, "out")
+#endif
+      call decomp_2d_write_one(1, phi1, 'out', 'phi1.dat', 0, io_name)
+      call decomp_2d_write_one(1, dphiX, 'out', 'dphiX.dat', 0, io_name)
+      call decomp_2d_write_one(1, dphiY, 'out', 'dphiY.dat', 0, io_name)
+      call decomp_2d_write_one(1, dphiZ, 'out', 'dphiZ.dat', 0, io_name)
+#ifdef ADIOS2
+      call decomp_2d_end_io(io_name, "out")
+      call decomp_2d_close_io(io_name, "out")
+#else
+      call write_xdmf()
+#endif
+
+   end subroutine write_data
+   !=====================================================================
+   ! Write of the xdmf file to visualise in paraview
+   !=====================================================================
+   subroutine write_xdmf()
+      ! This subroutine is based on the xdmf writers in Xcompact3d.
+      ! Copyright (c) 2012-2022, Xcompact3d
+      ! SPDX-License-Identifier: BSD 3-Clause
+
+      integer :: ioxdmf
+
+      character(len=:), allocatable :: fmt
+
+      integer :: precision
+
+      integer :: varctr
+      character(len=16) :: filename
+      character(len=5) :: varname
+      if (nrank == 0) then
+         OPEN (newunit=ioxdmf, file="./out.xdmf")
+
+         write (ioxdmf, '(A22)') '<?xml version="1.0" ?>'
+         write (ioxdmf, *) '<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>'
+         write (ioxdmf, *) '<Xdmf xmlns:xi="http://www.w3.org/2001/XInclude" Version="2.0">'
+         write (ioxdmf, *) '<Domain>'
+
+         write (ioxdmf, '(A)') '    <Topology name="topo" TopologyType="3DCoRectMesh"'
+         fmt = "(A, I0, A, I0, A, I0, A)"
+         write (ioxdmf, fmt) '        Dimensions="', nz, " ", ny, " ", nx, '">'
+         write (ioxdmf, '(A)') '    </Topology>'
+
+         write (ioxdmf, *) '    <Geometry name="geo" Type="ORIGIN_DXDYDZ">'
+         write (ioxdmf, *) '        <!-- Origin -->'
+         write (ioxdmf, *) '        <DataItem Format="XML" Dimensions="3">'
+         write (ioxdmf, *) '          0.0 0.0 0.0'
+         write (ioxdmf, *) '        </DataItem>'
+         write (ioxdmf, *) '        <!-- DxDyDz -->'
+         write (ioxdmf, *) '        <DataItem Format="XML" Dimensions="3">'
+         if (mytype == kind(0.0d0)) then
+            fmt = "(A, E24.17, A, E24.17, A, E24.17)"
+         else
+            fmt = "(A, E16.9, A, E16.9, A, E16.9)"
+         end if
+         write (ioxdmf, fmt) '        ', 1.0_mytype, " ", 1.0_mytype, " ", 1.0_mytype
+         write (ioxdmf, *) '        </DataItem>'
+         write (ioxdmf, *) '    </Geometry>'
+
+         write (ioxdmf, *) '   <Grid Name="1" GridType="Uniform">'
+         write (ioxdmf, *) '       <Topology Reference="/Xdmf/Domain/Topology[1]"/>'
+         write (ioxdmf, *) '       <Geometry Reference="/Xdmf/Domain/Geometry[1]"/>'
+         do varctr = 1, 4
+            select case (varctr)
+              case (1)
+                write (varname, "(A)") "phi1"
+                write (filename, '(A)') "./out/phi1.dat"
+              case (2)
+                write (varname, "(A)") "dphiX"
+                write (filename, '(A)') "./out/dphiX.dat"
+              case (3)
+                write (varname, "(A)") "dphiY"
+                write (filename, '(A)') "./out/dphiY.dat"
+              case (4)
+                write (varname, "(A)") "dphiZ"
+                write (filename, '(A)') "./out/dphiZ.dat"
+            end select
+            write (ioxdmf, *) '       <Attribute Name="'//trim(varname)//'" Center="Node">'
+#ifndef ADIOS2
+            write (ioxdmf, *) '          <DataItem Format="Binary"'
+#else
+            write (ioxdmf, *) '          <DataItem Format="HDF"'
+#endif
+
+#ifdef DOUBLE_PREC
+            print *, "Double precision build"
+#ifdef SAVE_SINGLE
+            if (output2D == 0) then
+               precision = 4
+            else
+               precision = 8
+            end if
+#else
+            precision = 8
+#endif
+#else
+            precision = 4
+#endif
+            write (ioxdmf, "(A,I0,A)") '            DataType="Float" Precision="', precision, '" Endian="little" Seek="0"'
+
+            fmt = "(A, I0, A, I0, A, I0, A)"
+            write (ioxdmf, fmt) '            Dimensions="', nz, " ", ny, " ", nx, '">'
+
+            write (ioxdmf, *) '              '//trim(filename)
+
+            write (ioxdmf, *) '           </DataItem>'
+            write (ioxdmf, *) '        </Attribute>'
+         end do
+         write (ioxdmf, '(/)')
+         write (ioxdmf, *) '    </Grid>'
+         write (ioxdmf, *) '</Domain>'
+         write (ioxdmf, '(A7)') '</Xdmf>'
+         close (ioxdmf)
+      end if
+
+   end subroutine write_xdmf
 end program grad3d

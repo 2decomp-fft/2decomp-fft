cca9fd9a42171a45e64ba4513d401544319a0a0f
rfj82982 <stefano.rolfo@stfc.ac.uk>
2024-02-29 08:07:28 +0000

    Update Intel LLVM options and modify the halo to correct bus and work with intel

--- cmake/compilers/D2D_flags_intel.cmake
+++ cmake/compilers/D2D_flags_intel.cmake
@@ -1,8 +1,10 @@
 # Compilers Flags for Intel
 # Check is the compiler is the new ifx based on LLVM or the old ifort
 if (Fortran_COMPILER_NAME MATCHES "IntelLLVM")
-  set(D2D_FFLAGS "-fpp -std08")
-  set(D2D_FFLAGS_RELEASE "-O2")
+  set(D2D_FFLAGS "-fpp -std08 -safe-cray-ptr -g -traceback")
+  set(D2D_FFLAGS_RELEASE "-O3")
+  #set(D2D_FFLAGS "-fpp -std08 -xHost -heaparrays -safe-cray-ptr -g -traceback")
+  #set(D2D_FFLAGS_RELEASE "-O3 -ipo")
 else (Fortran_COMPILER_NAME MATCHES "IntelLLVM")
   #set(CMAKE_Fortran_FLAGS "-cpp xSSE4.2 -axAVX,CORE-AVX-I,CORE-AVX2 -ipo -fp-model fast=2 -mcmodel=large -safe-cray-ptr")
   set(D2D_FFLAGS "-fpp -std08 -xHost -heaparrays -safe-cray-ptr -g -traceback")

33c3e216ade0fb051d30100e0798d5d84361abe5
cflag <cedric.flageul@univ-poitiers.fr>
2024-05-05 13:13:55 +0200

    Add a dummy file when ADIOS2 is not available

---
+++ src/io_adios_none.f90
@@ -0,0 +1,50 @@
+!! SPDX-License-Identifier: BSD-3-Clause
+
+! Preprocessor macro to deal with unused variables
+#define unused(x) associate(tmp => x); end associate
+
+!
+! Dummy module when there is no ADIOS2 IO available
+!
+module decomp_2d_io_adios
+
+   use decomp_2d
+   use decomp_2d_constants
+   use decomp_2d_io_utilities
+   use decomp_2d_mpi
+   use decomp_2d_profiler
+   use MPI
+   use, intrinsic :: iso_fortran_env, only: real32, real64
+
+   implicit none
+
+   private
+
+   public :: decomp_2d_io_adios_init, &
+             decomp_2d_io_adios_fin
+
+contains
+
+   !
+   ! Initialize the dummy ADIOS2 IO module
+   !
+   subroutine decomp_2d_io_adios_init(adios_xml)
+
+      implicit none
+
+      character(len=*), intent(in), optional :: adios_xml
+
+      unused(adios_xml)
+
+   end subroutine decomp_2d_io_adios_init
+
+   !
+   ! Finalize the dummy ADIOS2 IO module
+   !
+   subroutine decomp_2d_io_adios_fin()
+
+      implicit none
+
+   end subroutine decomp_2d_io_adios_fin
+
+end module decomp_2d_io_adios

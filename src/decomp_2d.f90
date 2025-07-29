0a4252ea6a3f97f19230a3af5ebf08e6367f1b9d
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2025-07-29 20:51:21 +0100

    Apparently ifdef isn't valid in Fortran...

--- src/decomp_2d.f90
+++ src/decomp_2d.f90
@@ -1209,7 +1209,7 @@ contains
    ! Utility routines to help allocate 3D arrays
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #include "alloc.f90"
-#ifdef(_GPU)
+#if defined(_GPU)
 #include "alloc_dev.f90"
 #endif
 
